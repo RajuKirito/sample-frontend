78cefb7dca86e890aed92a9e0ca09666a8a66887
RajuKirito <43929838+RajuKirito@users.noreply.github.com>
2023-01-07 10:13:16 +0530

    Rename index.html to index.js

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ console.log("Hello World");
 const hello = document.getElementById("hello");
 
 hello.onclick = ()=>{console.log("clicked")};
+
+const baseUrl = `sample-backend-production.up.railway.app`;
+
