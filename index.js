1833a3ba465a7c90d3f401028100c2d72a5534ce
RajuKirito <43929838+RajuKirito@users.noreply.github.com>
2023-01-07 10:29:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const hello = document.getElementById("hello");
 
 hello.onclick = ()=>{console.log("clicked")};
 
-const baseUrl = `sample-backend-production.up.railway.app`;
+const baseUrl = "sample-backend-production.up.railway.app";
 
 window.onload = ()=>{axios.get(baseUrl).then((res)=>console.log(res))};
