f568c0665f4882c16ce0fab4796e5a7d55f48fdd
RajuKirito <43929838+RajuKirito@users.noreply.github.com>
2023-01-08 19:07:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const hello = document.getElementById("hello");
 
 hello.onclick = ()=>{console.log("clicked")};
 
-const baseUrl = "https://sample-backend-production.up.railway.app";
+const baseUrl = "https://qlg4skavilmrb6vpdsyfgd72fi0jrfmi.lambda-url.ap-southeast-1.on.aws/";
 
 window.onload = ()=>{axios.get(baseUrl).then((res)=>console.log(res))};
